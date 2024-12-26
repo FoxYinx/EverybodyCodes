dacd1736baf6b120cf268aba359c252a75dca61f
joshuadwightred <tollemerkevin@gmail.com>
2024-12-26 17:55:36 +0100

    Year 2024 - Day 2 - Part 2

--- Year2024/day2/puzzle2.cpp
+++ Year2024/day2/puzzle2.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <regex>
 
 using namespace std;
 
@@ -13,9 +15,34 @@ int year2024_day2_puzzle2() {
     cout << "File successfully opened!" << endl;
 
     string s;
-    while (getline(f, s)) {
+    vector<string> runes;
+    getline(f, s);
+    s.insert(s.end(),',');
+    const regex regexp(R"((\w+),)");
+    smatch sm;
+    while (regex_search(s, sm, regexp)) {
+        runes.push_back(sm[1]);
+        s = sm.suffix();
+    }
+    getline(f, s);
 
+    int validRunes = 0;
+    while (getline(f, s)) {
+        vector flags(s.size(), false);
+        for (int i = 0; i < s.size(); i++) {
+            for (const string& rune : runes) {
+                string reversed_rune = rune;
+                reverse(reversed_rune.begin(), reversed_rune.end());
+                if (s.size() - i >= rune.size() && (rune == s.substr(i, rune.size()) || reversed_rune == s.substr(i, rune.size()))) {
+                    for (int j = 0; j < rune.size(); j++) {
+                        flags[i + j] = true;
+                    }
+                }
+            }
+        }
+        for (const auto& flag : flags) if (flag) validRunes++;
     }
 
+    cout << validRunes << endl;
     return 0;
 }
