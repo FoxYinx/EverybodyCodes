1019443524674daa39c59744d79a5f2e2fd95e6f
joshuadwightred <tollemerkevin@gmail.com>
2024-12-26 16:00:29 +0100

    Initial commit

---
+++ Year2024/day1/puzzle1.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int year2024_day1_puzzle1() {
+    ifstream f("ressources/Year2024/day1.txt");
+
+    if (!f.is_open()) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+    cout << "File successfully opened!" << endl;
+
+    string s;
+    while (getline(f, s)) {
+
+    }
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+// Function declarations
+int year2024_day1_puzzle1();
+int year2024_day1_puzzle2();
+int year2024_day1_puzzle3();
+
+// Function registry
+map<string, function<int()> > functions = {
+    {"2024-1-1", year2024_day1_puzzle1},
+    {"2024-1-2", year2024_day1_puzzle2},
+    {"2024-1-3", year2024_day1_puzzle3},
+};
+
+int main() {
+    int year, day, puzzle;
+    cout << "Enter which year: ";
+    cin >> year;
+    cout << "Enter which day: ";
+    cin >> day;
+    cout << "Enter which puzzle (1 or 2): ";
+    cin >> puzzle;
+    if (const string key = to_string(year) + "-" + to_string(day) + "-" + to_string(puzzle); functions.contains(key)) {
+        std::cout << "Running Year " << year << " Day " << day << " Part " << puzzle << "...\n";
+        return functions[key](); // Call the corresponding function
+    }
+    std::cout << "Invalid day or part. Please check your input.\n";
+    return 1;
+}
