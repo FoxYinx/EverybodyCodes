3f074bce1a8fd807ce86ba073b20e106cf660b3f
joshuadwightred <tollemerkevin@gmail.com>
2024-12-26 16:37:11 +0100

    Year 2024 - Day 1 - Part 3

--- Year2024/day1/puzzle3.cpp
+++ Year2024/day1/puzzle3.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include "utils.h"
 
 using namespace std;
 
@@ -13,9 +14,22 @@ int year2024_day1_puzzle3() {
     cout << "File successfully opened!" << endl;
 
     string s;
-    while (getline(f, s)) {
+    int potions = 0;
+    getline(f, s);
+    for (int i = 0; i < s.size(); i += 3) {
+        const char c1 = s.at(i);
+        const char c2 = s.at(i + 1);
+        const char c3 = s.at(i + 2);
+        potions += processChar(c1) + processChar(c2) + processChar(c3);
 
+        int nbX = 0;
+        if (c1 == 'x') nbX++;
+        if (c2 == 'x') nbX++;
+        if (c3 == 'x') nbX++;
+        if (nbX == 1) potions += 2;
+        else if (nbX == 0) potions += 6;
     }
 
+    cout << potions << endl;
     return 0;
 }
