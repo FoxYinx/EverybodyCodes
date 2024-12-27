b998dbeb65fe29399f7e2d291a2172e0e6773ad7
joshuadwightred <tollemerkevin@gmail.com>
2024-12-27 19:42:45 +0100

    Year 2024 - Day 5 - Part 3

--- Year2024/day5/puzzle3.cpp
+++ Year2024/day5/puzzle3.cpp
@@ -1,5 +1,11 @@
 #include <fstream>
 #include <iostream>
+#include <array>
+#include <cstdint>
+#include <vector>
+
+#define COLUMNNB 4
+#define ROUNDNB 1000
 
 using namespace std;
 
@@ -13,10 +19,42 @@ int year2024_day5_puzzle3() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    array<vector<int>, COLUMNNB> clappers;
     while (getline(f, s)) {
-
+        for (int i = 0; i < COLUMNNB; i++) {
+            try {
+                clappers[i].push_back(stoi(s.substr(i * 5, 5)));
+            } catch (const invalid_argument& e) {
+                cerr << "Invalid argument: " << e.what() << endl;
+                return 1;
+            } catch (const out_of_range& e) {
+                cerr << "Out of range: " << e.what() << endl;
+                return 1;
+            }
+        }
     }
 
+    uint64_t hightestNumber = 0;
+
+    for (int round = 0; round < ROUNDNB; round++) {
+        int clapperValue = clappers[round % COLUMNNB].front();
+        int clapper = clappers[round % COLUMNNB].front() % (clappers[(round + 1) % COLUMNNB].size() * 2);
+        if (clapper == 0) clapper = clappers[(round + 1) % COLUMNNB].size() * 2;
+        clappers[round % COLUMNNB].erase(clappers[round % COLUMNNB].begin());
+        if (clapper > clappers[(round + 1) % COLUMNNB].size()) {
+            clappers[(round + 1) % COLUMNNB].insert(clappers[(round + 1) % COLUMNNB].end() - (clapper - 1 - clappers[(round + 1) % COLUMNNB].size()), clapperValue);
+        } else {
+            clappers[(round + 1) % COLUMNNB].insert(clappers[(round + 1) % COLUMNNB].begin() + clapper - 1, clapperValue);
+        }
+
+        string temp;
+        for (const auto& c : clappers) {
+            temp += to_string(c[0]);
+        }
+        if (const uint64_t number = stoull(temp); number > hightestNumber) hightestNumber = number;
+    }
+
+    cout << hightestNumber << endl;
     return 0;
 }
 
