5e2d2b1d4e855e05e69f3b501ffcf283a8d4b073
joshuadwightred <tollemerkevin@gmail.com>
2024-12-27 14:18:15 +0100

    Year 2024 - Day 5

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ int year2024_day3_puzzle3();
 int year2024_day4_puzzle1();
 int year2024_day4_puzzle2();
 int year2024_day4_puzzle3();
+int year2024_day5_puzzle1();
+int year2024_day5_puzzle2();
+int year2024_day5_puzzle3();
 
 // Function registry
 map<string, function<int()> > functions = {
@@ -33,6 +36,9 @@ map<string, function<int()> > functions = {
     {"2024-4-1", year2024_day4_puzzle1},
     {"2024-4-2", year2024_day4_puzzle2},
     {"2024-4-3", year2024_day4_puzzle3},
+    {"2024-5-1", year2024_day5_puzzle1},
+    {"2024-5-2", year2024_day5_puzzle2},
+    {"2024-5-3", year2024_day5_puzzle3},
 };
 
 int main() {
