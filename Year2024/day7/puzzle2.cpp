e34734c8fff3a48edd526e5ec5adb5de5b739dc2
joshuadwightred <tollemerkevin@gmail.com>
2024-12-28 16:58:42 +0100

    Year 2024 - Day 7 - Part 2

--- Year2024/day7/puzzle2.cpp
+++ Year2024/day7/puzzle2.cpp
@@ -1,7 +1,21 @@
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <vector>
+#include <regex>
+
+#define LAPS 10
 
 using namespace std;
+
+struct Node {
+    string name;
+    int value;
+
+    bool operator<(const Node& other) const {
+        return value > other.value;
+    }
+};
 
 int year2024_day7_puzzle2() {
     ifstream f("ressources/Year2024/day7/part2.txt");
@@ -13,8 +27,53 @@ int year2024_day7_puzzle2() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    const regex regexp(R"((\w+):)");
+    const regex regexp2(R"((\+|\-|\=),)");
+    smatch sm;
+    map<string, vector<string>> devices;
     while (getline(f, s)) {
+        s.insert(s.end(), ',');
+        string key;
+        if (regex_search(s, sm, regexp)) {
+            key = sm[1];
+        }
+        vector<string> values;
+        while (regex_search(s, sm, regexp2)) {
+            values.push_back(sm[1]);
+            s = sm.suffix();
+        }
+        devices[key] = values;
+    }
 
+    vector<Node> output;
+    string track = "S-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=---=++==--+++==++=+=--==++==+++=++=+++=--=+=-=+=-+=-+=-+-=+=-=+=-+++=+==++++==---=+=+=-";
+
+    for (const auto &[key, value] : devices) {
+        int result = 0;
+        int temp = 10;
+        for (int i = 0; i < track.size() * LAPS; i++) {
+            const string trackOp(1, track[(i + 1) % track.size()]);
+            const string& racerOp = value[i % value.size()];
+            if (trackOp == "S" || trackOp == "=") {
+                if (racerOp == "+") {
+                    temp++;
+                } else if (racerOp == "-") {
+                    if (temp > 0) temp--;
+                }
+            } else if (trackOp == "+") {
+                temp++;
+            } else {
+                if (temp > 0) temp--;
+            }
+            result += temp;
+        }
+        output.emplace_back(key, result);
+    }
+
+    sort(output.begin(), output.end());
+
+    for (const auto&[name, _] : output) {
+        cout << name;
     }
 
     return 0;
