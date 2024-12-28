5e32e79b9d37d3cbae34e36d5099805848b56e74
joshuadwightred <tollemerkevin@gmail.com>
2024-12-28 22:00:24 +0100

    Year 2024 - Day 8 - Part 3

--- Year2024/day8/puzzle3.cpp
+++ Year2024/day8/puzzle3.cpp
@@ -1,5 +1,10 @@
 #include <fstream>
 #include <iostream>
+#include <numeric>
+#include <vector>
+
+#define ACOLYTES 10
+#define BLOCKS 202400000
 
 using namespace std;
 
@@ -13,9 +18,23 @@ int year2024_day8_puzzle3() {
     cout << "File successfully opened!" << endl;
 
     string s;
-    while (getline(f, s)) {
+    getline(f, s);
+    const int input = stoi(s);
 
+    vector pyramid = {1};
+
+    while (accumulate(pyramid.begin(), pyramid.end(), 0) < BLOCKS) {
+        int newthickness = pyramid[0] * input % ACOLYTES + ACOLYTES;
+        for (int& value : pyramid) value += newthickness;
+        pyramid.insert(pyramid.begin(), newthickness);
+        pyramid.insert(pyramid.end(), newthickness);
     }
 
+    const unsigned long long& size = pyramid.size();
+    for (int i = 1; i < size - 1; i++) {
+        pyramid[i] -= input * size * pyramid[i] % ACOLYTES;
+    }
+
+    cout << accumulate(pyramid.begin(), pyramid.end(), 0) - BLOCKS << endl;
     return 0;
 }
