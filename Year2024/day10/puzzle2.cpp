a914cd39a619e70af9a0710acae2ae869af47ff8
joshuadwightred <tollemerkevin@gmail.com>
2024-12-29 18:23:56 +0100

    Year 2024 - Day 10 - Part 2 (ongoing)

--- Year2024/day10/puzzle2.cpp
+++ Year2024/day10/puzzle2.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <array>
+#include "utils.h"
 
 using namespace std;
 
@@ -12,10 +14,30 @@ int year2024_day10_puzzle2() {
     }
     cout << "File successfully opened!" << endl;
 
-    string s;
-    while (getline(f, s)) {
+    array<array<char, SIZE>, SIZE> map = {};
 
+    string s;
+    int line = 0;
+    while (getline(f, s)) {
+        for (int i = 0; i < SIZE; i++) {
+            map[line][i] = s[i];
+        }
+        line++;
     }
 
+    int power = 0;
+    int pos = 1;
+    for (int y = 0; y < SIZE; y++) {
+        for (int x = 0; x < SIZE; x++) {
+            if (map[y][x] == '.') {
+                const char newLetter = findLetter(map, y, x);
+                power += (newLetter - '@') * pos;
+                map[y][x] = newLetter;
+                pos++;
+            }
+        }
+    }
+
+    cout << power << endl;
     return 0;
 }
