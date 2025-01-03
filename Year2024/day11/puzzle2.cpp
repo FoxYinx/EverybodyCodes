d1dd002cd88d12652f54ad90d2c1296f0d32d7a4
yinx <tollemerkevin@gmail.com>
2025-01-03 11:51:32 +0100

    Year 2024 - Day 11 - Part 2

--- Year2024/day11/puzzle2.cpp
+++ Year2024/day11/puzzle2.cpp
@@ -1,5 +1,9 @@
 #include <fstream>
 #include <iostream>
+#include <regex>
+#include "utils.h"
+
+#define ITER 10
 
 using namespace std;
 
@@ -13,9 +17,25 @@ int year2024_day11_puzzle2() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    map<string, vector<string>> evolution;
+    smatch sm;
+    const regex regexp1(R"((\w+):)");
+    const regex regexp2(R"((\w+),)");
     while (getline(f, s)) {
-
+        s.insert(s.end(), ',');
+        string key;
+        vector<string> temp;
+        if (regex_search(s, sm, regexp1)) {
+            key = sm[1];
+        }
+        while (regex_search(s, sm, regexp2)) {
+            temp.push_back(sm[1]);
+            s = sm.suffix();
+        }
+        evolution[key] = temp;
     }
 
+    int population = runFullEvolution(ITER, evolution, "Z");
+    cout << population << endl;
     return 0;
 }
