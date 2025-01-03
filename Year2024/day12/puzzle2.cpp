4de36afbe0af03c526c99c2a0e03101c146138a0
yinx <tollemerkevin@gmail.com>
2025-01-03 14:40:01 +0100

    Year 2024 - Day 12 - Part 2

--- Year2024/day12/puzzle2.cpp
+++ Year2024/day12/puzzle2.cpp
@@ -1,5 +1,12 @@
+#include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <ranges>
+#include "utils.h"
+
+#define HEIGHT 21
+#define LENGTH 99
 
 using namespace std;
 
@@ -13,9 +20,36 @@ int year2024_day12_puzzle2() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    map<char, Pos> catapultes;
+    map<Pos, int> targets;
+    int line = 0;
     while (getline(f, s)) {
+        line++;
 
+        for (int i = 0; i < LENGTH; i++) {
+            if (s[i] == 'T') {
+                targets[{i, HEIGHT - line}] = 1;
+            } else if (s[i] == 'H') {
+                targets[{i, HEIGHT - line}] = 2;
+            } else if (s[i] != '.' && s[i] != '=') {
+                catapultes[s[i]] = {i, HEIGHT - line};
+            }
+        }
     }
 
+    uint64_t totalPower = 0;
+
+    for (Pos pos : targets | views::keys) {
+        for (int power = 1; power < LENGTH / 2; power++) {
+            for (const auto &[key, value] : catapultes) {
+                if (checkIfHit(value, power, pos)) {
+                    //cout << key << " hits a tower with power (" << power << ")" << endl;
+                    totalPower += power * (key - '@') * targets[pos];
+                }
+            }
+        }
+    }
+
+    cout << totalPower << endl;
     return 0;
 }
