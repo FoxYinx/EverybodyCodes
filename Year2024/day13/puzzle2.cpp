3bff2074427ecff66ab9857c4bcda67ec88a306b
yinx <tollemerkevin@gmail.com>
2025-01-04 13:28:35 +0100

    Year 2024 - Day 13 - Part 2

--- Year2024/day13/puzzle2.cpp
+++ Year2024/day13/puzzle2.cpp
@@ -1,5 +1,8 @@
+#include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include "utils.h"
 
 using namespace std;
 
@@ -13,9 +16,26 @@ int year2024_day13_puzzle2() {
     cout << "File successfully opened!" << endl;
 
     string s;
+    vector<vector<Node>> map;
+    int line = 0;
+    Node start = {};
     while(getline(f, s)) {
-
+        vector<Node> temp;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'S') {
+                start = {i, line, s[i], 0};
+                temp.push_back({i, line, s[i], 0});
+            } else if (s[i] == 'E') {
+                temp.push_back({i, line, s[i], 0});
+            } else {
+                temp.push_back({i, line, s[i], static_cast<uint64_t>(s[i] - '0')});
+            }
+        }
+        map.push_back(temp);
+        line++;
     }
 
+    uint64_t fastest = dijkstra(map, start);
+    cout << fastest << endl;
     return 0;
 }
