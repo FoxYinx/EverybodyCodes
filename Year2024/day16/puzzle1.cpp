4da229baac10f6fb85b02402e7ea9dd5ff0b7e42
yinx <tollemerkevin@gmail.com>
2025-01-12 19:27:24 +0100

    Year 2024 - Day 16 - Part 1

--- Year2024/day16/puzzle1.cpp
+++ Year2024/day16/puzzle1.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <regex>
+#include <ranges>
 
 using namespace std;
 
@@ -13,8 +15,41 @@ int year2024_day16_puzzle1() {
     cout << "File successfully opened!" << endl;
 
     string s;
-    while (getline(f, s)) {
+    smatch sm;
+    const regex regexp1(R"((\d+))");
+    const regex regexp2(R"(([*^><-][_.:,][><*-^]| {4}))");
 
+    vector<int> nbRotations;
+    getline(f, s);
+    while (regex_search(s, sm, regexp1)) {
+        nbRotations.push_back(stoi(sm[1]));
+        s = sm.suffix();
+    }
+    getline(f, s);
+
+    map<int, vector<string>> slotMachine;
+    while (getline(f, s)) {
+        int col = 0;
+        while (regex_search(s, sm, regexp2)) {
+            if (sm[1] != "    ") slotMachine[col].push_back(sm[1]);
+            s = sm.suffix();
+            col++;
+        }
+    }
+
+    map<int, int> pos;
+    for (const int& key: slotMachine | views::keys) {
+        pos[key] = 0;
+    }
+
+    for (auto &[key, value] : pos) {
+        for (int i = 1; i <= 100; i++) {
+            pos[key] = (value + nbRotations[key]) % slotMachine[key].size();
+        }
+    }
+
+    for (auto &[key, value] : slotMachine) {
+        cout << value[pos[key]] << " ";
     }
 
     return 0;
