30fe1d840296f1b8a4cf19fa9303e7213f94bc24
yinx <tollemerkevin@gmail.com>
2025-01-12 16:29:24 +0100

    Year 2024 - Day 16

--- main.cpp
+++ main.cpp
@@ -51,6 +51,10 @@ int year2024_day14_puzzle3();
 int year2024_day15_puzzle1();
 int year2024_day15_puzzle2();
 int year2024_day15_puzzle3();
+int year2024_day16_puzzle1();
+int year2024_day16_puzzle2();
+int year2024_day16_puzzle3();
+
 
 
 // Function registry
@@ -100,6 +104,9 @@ map<string, function<int()> > functions = {
     {"2024-15-1", year2024_day15_puzzle1},
     {"2024-15-2", year2024_day15_puzzle2},
     {"2024-15-3", year2024_day15_puzzle3},
+    {"2024-16-1", year2024_day16_puzzle1},
+    {"2024-16-2", year2024_day16_puzzle2},
+    {"2024-16-3", year2024_day16_puzzle3},
 };
 
 int main() {
